3e6047c25a3db7ec8e0e7b2efdbe34c5b487b6c2
Miles Watkins <miles.w.watkins@gmail.com>
2018-01-22 12:05:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+Slack State Flags
+---
+
+Add state flags as custom emoji to your Slack.
+
+### Uploading to Slack
+
+Upload the PNGs to Slack, to create new emoji, using a simple tool such as https://github.com/smashwilson/slack-emojinator
+
+```bash
+git clone https://github.com/smashwilson/slack-emojinator.git
+cd slack-emojinator
+mkvirtualenv slack-emojinator
+pip install -r requirements.txt
+
+# Store your Slack's team name and most recent cookie in environment variables
+export SLACK_TEAM=${YOUR_TEAM_NAME}
+export SLACK_COOKIE=${YOUR_SLACK_COOKIE}
+python upload.py ../state-flags/*
+```
+
+### Gathering the flags yourself
+
+I've included the flag PNGs in this repository, so this step isn't necessary. But if you want to see where they came from, or tweak this script for your preferred country, read on!
+
+#### Requirements
+
+- libxml
+- Python 3
+- `pip install -r requirements`
+
+#### Running
+
+```
+$ python get_flags.py
+Downloading flag for Alabama
+Downloading flag for Alaska
+Downloading flag for American Samoa
+Downloading flag for Arizona
+Downloading flag for Arkansas
+Downloading flag for California
+…
+```
+
+### Source
+
+Flag images sourced libre from Wikipedia.
---
+++ get_flags.py
@@ -0,0 +1,44 @@
+import os
+import re
+import shutil
+
+import lxml.html
+import requests
+import us
+
+
+FLAGS_URL = 'https://en.wikipedia.org/wiki/Flags_of_the_U.S._states_and_territories'
+html = requests.get(FLAGS_URL).text
+doc = lxml.html.fromstring(html)
+doc.make_links_absolute(FLAGS_URL)
+
+STATES_XPATH = '//span[@id="Current_state_flags"]/parent::h2/following-sibling::table[1]//img[contains(@src, "Flag_of")]'
+TERRITORIES_XPATH = '//span[@id="Current_federal_district_and_territory_flags"]/parent::h2/following-sibling::table[1]//img[contains(@src, "Flag_of")]'
+flag_elems = doc.xpath(STATES_XPATH) + doc.xpath(TERRITORIES_XPATH)
+
+# Given a max dimension, you can change a Wikipedia-rendered SVG-as-PNG URL
+# to your desired size
+MAX_SLACK_IMAGE_DIMENSION = 128
+flags = {
+    e.attrib['alt']: re.sub(r'\/\d+px', '/{}px'.format(MAX_SLACK_IMAGE_DIMENSION), e.attrib['src'])
+    for e in flag_elems
+}
+
+DIRECTORY = 'state-flags'
+states = [s for s in us.STATES_AND_TERRITORIES if s not in us.OBSOLETE]
+for state in states:
+    print("Downloading flag for {}".format(state.name))
+
+    filename = 'us-{}.png'.format(state.abbr.lower())
+    png_url = None
+    for name, url in flags.items():
+        if state.name in name:
+            png_url = url
+            break
+    else:
+        raise ValueError("Did not find flag for {}".format(state.name))
+
+    flag_stream = requests.get(png_url, stream=True)
+    with open(os.path.join(DIRECTORY, filename), 'wb') as flag_file:
+        shutil.copyfileobj(flag_stream.raw, flag_file)
+    del flag_stream
